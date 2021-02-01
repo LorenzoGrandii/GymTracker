6b208c30837d20394372d17ffdd9274b14d5dd11
BuildTools <unconfigured@null.spigotmc.org>
2021-02-01 20:39:30 +0100

    Sono stati tolti i metodi onresume, onrestart, ecc...  Se da problemi con l'inflate della home, reinserirli

--- app/src/main/java/com/grandi/lorenzo/gymtracker/home/HomeActivity.java
+++ app/src/main/java/com/grandi/lorenzo/gymtracker/home/HomeActivity.java
@@ -4,9 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -24,7 +25,7 @@ import static com.grandi.lorenzo.gymtracker.KeyLoader.*;
 public class HomeActivity extends FragmentActivity {
 
     private TextView tv_date, tv_name, tv_account_id;
-    private AppCompatImageButton acib_profile, acib_scanner, acib_settings, acib_training;
+    private ImageButton ib_profile, ib_scanner, ib_settings, ib_training;
 
     private String name, account_id;
     private boolean training;
@@ -65,45 +66,47 @@ public class HomeActivity extends FragmentActivity {
         editor.apply();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void initViewComponents(Activity activity) {
         this.tv_name = activity.findViewById(R.id.tv_name);
         this.tv_account_id = activity.findViewById(R.id.tvLogin);
         this.tv_date = activity.findViewById(R.id.tv_date);
 
-        this.acib_profile = activity.findViewById(R.id.profile_opener);
-        this.acib_scanner = activity.findViewById(R.id.scanner_opener);
-        this.acib_training = activity.findViewById(R.id.training_opener);
-        this.acib_settings = activity.findViewById(R.id.settings_opener);
+        this.ib_profile = activity.findViewById(R.id.profile_opener);
+        this.ib_scanner = activity.findViewById(R.id.scanner_opener);
+        this.ib_training = activity.findViewById(R.id.training_opener);
+        this.ib_settings = activity.findViewById(R.id.settings_opener);
+        trainingStatus(this.training);
 
-        this.acib_profile.setOnClickListener(v -> {
+        this.ib_profile.setOnClickListener(v -> {
             preferenceSaver(this);
-            this.acib_profile.setPressed(true);
+            this.ib_profile.setPressed(true);
 
             Intent intent = new Intent(this, TaskActivityHandler.class);
             new FlagList(intent);
             intent.putExtra(EXTRA_TASK_SELECTOR.getValue(), "profile");
             startActivity(intent);
         });
-        this.acib_scanner.setOnClickListener(v -> {
+        this.ib_scanner.setOnClickListener(v -> {
             preferenceSaver(this);
-            this.acib_scanner.setPressed(true);
+            this.ib_scanner.setPressed(true);
 
             Intent intent = new Intent(this, ScannerActivity.class);
             new FlagList(intent);
             startActivity(intent);
         });
-        this.acib_training.setOnClickListener(v -> {
+        this.ib_training.setOnClickListener(v -> {
             preferenceSaver(this);
-            this.acib_training.setPressed(true);
+            this.ib_training.setPressed(true);
 
             Intent intent = new Intent(this, TaskActivityHandler.class);
             new FlagList(intent);
             intent.putExtra(EXTRA_TASK_SELECTOR.getValue(), "training");
             startActivity(intent);
         });
-        this.acib_settings.setOnClickListener(v -> {
+        this.ib_settings.setOnClickListener(v -> {
             preferenceSaver(this);
-            this.acib_settings.setPressed(true);
+            this.ib_settings.setPressed(true);
 
             Intent intent = new Intent(this, TaskActivityHandler.class);
             new FlagList(intent);
@@ -121,9 +124,9 @@ public class HomeActivity extends FragmentActivity {
     @SuppressLint("UseCompatLoadingForDrawables")
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void trainingStatus(boolean training) {
-        if (training)
-            this.acib_scanner.setBackground(getDrawable(R.drawable.button_scanner_exit));
+        if (!training)
+            this.ib_scanner.setBackground(getDrawable(R.drawable.button_scanner_exit));
         else
-            this.acib_scanner.setBackground(getDrawable(R.drawable.button_scanner_enter));
+            this.ib_scanner.setBackground(getDrawable(R.drawable.button_scanner_enter));
     }
 }
