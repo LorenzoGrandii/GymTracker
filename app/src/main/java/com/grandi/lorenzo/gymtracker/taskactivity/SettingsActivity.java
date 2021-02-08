00fccc1268075b1f2ba44ead220e2a5eb7f288b8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-08 17:11:06 +0100

    Implementazione settings

--- app/src/main/java/com/grandi/lorenzo/gymtracker/taskactivity/SettingsActivity.java
+++ app/src/main/java/com/grandi/lorenzo/gymtracker/taskactivity/SettingsActivity.java
@@ -1,12 +1,18 @@
 package com.grandi.lorenzo.gymtracker.taskactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 
+import com.google.android.material.button.MaterialButton;
 import com.grandi.lorenzo.gymtracker.FlagList;
 import com.grandi.lorenzo.gymtracker.R;
 import com.grandi.lorenzo.gymtracker.home.HomeActivity;
@@ -17,13 +23,19 @@ public class SettingsActivity extends AppCompatActivity {
 
     private Spinner s_color;
     private String color;
+    private SwitchCompat sc_temperature, sc_stepCounter;
+    private boolean temperature_enabled, stepcounter_enabled;
+    private Button b_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        initViewComponents();
-        initTaskComponents();
+        if (this != null) {
+            initViewComponents();
+            initTaskComponents();
+        }
+
     }
 
     @Override
@@ -42,6 +54,8 @@ public class SettingsActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferencesLoader().edit();
         // TODO: save all preferences used
         editor.putString(colorKey.getValue(), this.color);
+        editor.putBoolean(temperatureKey.getValue(), this.temperature_enabled);
+        editor.putBoolean(stepCounterKey.getValue(), this.stepcounter_enabled);
 
         editor.apply();
     }
@@ -51,8 +65,29 @@ public class SettingsActivity extends AppCompatActivity {
         this.s_color = findViewById(R.id.s_color);
         this.color = String.valueOf(s_color.getSelectedItem());
 
+        this.sc_temperature = findViewById(R.id.temperature_switch);
+        this.sc_stepCounter = findViewById(R.id.step_counter_switch);
+
+        this.b_save = findViewById(R.id.settings_saver);
     }
     private void initTaskComponents() {
         // TODO: initialization for all task components
+        this.sc_temperature.setChecked(preferencesLoader().getBoolean(temperatureKey.getValue(), false));
+        this.sc_stepCounter.setChecked(preferencesLoader().getBoolean(stepCounterKey.getValue(), false));
+        changeListener();
+    }
+
+    private void changeListener() {
+        // return true on any setting change -> button state clickable and color primary set
+
+        this.sc_temperature.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            temperature_enabled = isChecked;
+        });
+        this.sc_stepCounter.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            stepcounter_enabled = isChecked;
+        });
+        b_save.setOnClickListener(v -> {
+            this.preferenceSaver();
+        });
     }
 }
