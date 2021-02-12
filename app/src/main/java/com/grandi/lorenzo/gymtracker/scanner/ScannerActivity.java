5b53079eada560921f42945f00a4f250f316b800
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 17:44:58 +0100

    Fine implementazione App

--- app/src/main/java/com/grandi/lorenzo/gymtracker/scanner/ScannerActivity.java
+++ app/src/main/java/com/grandi/lorenzo/gymtracker/scanner/ScannerActivity.java
@@ -11,20 +11,15 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
-import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.zxing.Result;
 import com.grandi.lorenzo.gymtracker.R;
 import com.grandi.lorenzo.gymtracker.task.CalendarHandler;
 
@@ -36,12 +31,14 @@ import static com.grandi.lorenzo.gymtracker.KeyLoader.*;
 public class ScannerActivity extends AppCompatActivity {
 
     private boolean trainingStatus;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
 
     private TextView tv_date_scanner;
+    private SurfaceView surfaceView;
     private Context context;
 
-    private CodeScanner codeScanner;
-    private CodeScannerView codeScannerView;
+    private CameraSource cameraSource;
+    private BarcodeDetector barcodeDetector;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -54,16 +51,16 @@ public class ScannerActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        super.onStop();
         preferenceSaver();
         qrCodeDispatcher();
+        super.onStop();
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
         preferenceSaver();
         qrCodeDispatcher();
+        super.onPause();
     }
 
     @Override
@@ -79,12 +76,6 @@ public class ScannerActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        codeScanner.startPreview();
-    }
-
     private SharedPreferences preferenceLoader() {
         return this.getSharedPreferences(LOGIN_PREFERENCE_FILE.getValue(), MODE_PRIVATE);
     }
@@ -97,40 +88,65 @@ public class ScannerActivity extends AppCompatActivity {
 
     private void initViewComponents() {
         this.tv_date_scanner = this.findViewById(R.id.tv_date_scanner);
-        Log.e("date_scanner","created");
-        codeScannerView = findViewById(R.id.previewView);
-        Log.e("previewView","created");
+        this.surfaceView = this.findViewById(R.id.previewView);
     }
     private void initTaskComponents() {
         this.context = this;
         this.trainingStatus = preferenceLoader().getBoolean(trainingKey.getValue(), false);
         this.tv_date_scanner.setText((new CalendarHandler()).getDate());
-        // this.cameraManager();
+        this.cameraManager();
+    }
 
-        codeScanner = new CodeScanner(this, codeScannerView);
-        Log.e("codeScanner","created");
-        codeScanner.setDecodeCallback(result -> runOnUiThread(() -> {
-            Log.e("callback","set");
-            if (result.getText().equals(strQRFlag.getValue())){
-                Log.e("Code right","");
-                SharedPreferences.Editor editor = preferenceLoader().edit();
-                editor.putBoolean(trainingKey.getValue(), !trainingStatus);
-                editor.apply();
-                registerEvent();
-                qrCodeDispatcher();
+    private void cameraManager() {
+        barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+        this.cameraSource = new CameraSource.Builder(this, this.barcodeDetector).setRequestedPreviewSize(1920, 1080).setAutoFocusEnabled(true).build();
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(ScannerActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                        cameraSource.start(surfaceView.getHolder());
+                    else
+                        ActivityCompat.requestPermissions(ScannerActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
             }
-        }));
-        codeScannerView.setOnClickListener(v -> {
-            codeScanner.startPreview();
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                cameraSource.stop();
+                cameraSource.release();
+            }
+        });
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                if (qrCodes.size()!=0 && qrCodes.valueAt(0).displayValue.equals(strQRFlag.getValue())) {
+                    SharedPreferences.Editor editor = preferenceLoader().edit();
+                    editor.putBoolean(trainingKey.getValue(), !trainingStatus);
+                    editor.apply();
+                    registerEvent();
+                    qrCodeDispatcher();
+                }
+            }
         });
     }
 
-
-
     private void qrCodeDispatcher() {
         preferenceSaver();
-        codeScanner.stopPreview();
-        codeScanner.releaseResources();
+        cameraSource.stop();
+        cameraSource.release();
+        barcodeDetector.release();
     }
 
     private void registerEvent() {
